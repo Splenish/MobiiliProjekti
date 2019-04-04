930e59a0bfb7b31da67941408ad38043adf69a11
t7jari00 <43604716+t7jari00@users.noreply.github.com>
2019-04-04 13:36:10 +0300

    better url ending

--- Python/dropbox_test.py
+++ Python/dropbox_test.py
@@ -33,7 +33,7 @@ class TransferData:
         picUrl = dbx.sharing_create_shared_link_with_settings(file_to).url
         print(picUrl)
 
-        picUrl = url[:-4]
+        picUrl = picUrl[:-4]
         picUrl += "?raw=1"
 
         firebase.put('traps/' + serialNumber,"triggered", True)
