857209ee561f9b664ad33900931bf16ca6b1b4e9
t7jari00 <t7jari00@students.oamk.fi>
2019-04-08 07:08:52 +0000

    GPS working and fixed url

--- Python/dropbox_test.py
+++ Python/dropbox_test.py
@@ -5,7 +5,9 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import string
 import random
+import serial
 from firebase import firebase
+from threading import Thread
 
 serialNumber = "01"
 random = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
@@ -31,23 +33,40 @@ class TransferData:
 
         print("getting url") 
         picUrl = dbx.sharing_create_shared_link_with_settings(file_to).url
+        picUrl = picUrl[:-4]
+        picUrl = picUrl + "raw=1"
+        
         print(picUrl)
 
         firebase.put('traps/' + serialNumber,"triggered", True)
-        firebase.put('traps/' + serialNumber,"url", picUrl)
+        firebase.put('traps/' + serialNumber,"urlString", picUrl)
 
-def take_picture(channel):
+def take_picture():
     print("Cat got")
     camera.capture('/home/pi/MobiiliProjekti/Python/picture_' + random + '.jpg')
     
     file_from = 'picture_' + random + '.jpg'
     file_to = '/Tunnistus/picture_' + random + '.jpg'
-
     transferData.upload_file(file_from, file_to)
+
+def button_pressed(channel):
+    if(firebase.get('traps/' + serialNumber,"triggered") == False):
+        print("Button pressed")
+        Thread(target=take_picture).start()
+    
 
 access_token = 'vh3aWw_IEX8AAAAAAAACVNfnHfGI82qV-I6sHVOlATnMuilvnyAhT1NSxrjhnQcg'
 transferData = TransferData(access_token)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(camPin, GPIO.IN)
-GPIO.add_event_detect(camPin,GPIO.RISING,callback=take_picture)
+GPIO.add_event_detect(camPin,GPIO.RISING,callback=button_pressed)
+ser = serial.Serial ("/dev/ttyACM0",9600)
+ser.baudrate=9600
+
+while ser.inWaiting:
+    GPSString = str(ser.readline())
+    GPSString = GPSString[2:]
+    GPSString = GPSString[:-5]
+    print(GPSString)
+    firebase.put('traps/' + serialNumber,"pos", GPSString)
