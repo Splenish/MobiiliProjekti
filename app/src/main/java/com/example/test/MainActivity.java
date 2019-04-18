4b7dbdc1c6232f1052d6eedcd637c5541d54c063
E-Man <t7auee00@students.oamk.fi>
2019-04-18 15:16:54 +0300

    PASKA

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -98,6 +100,31 @@ public class MainActivity extends AppCompatActivity {
         Log.d("SHOUTBOARD", "name: " + name);
         Log.d("SHOUTBOARD", "pic: " + profile_pic);
         Log.d("SHOUTBOARD", "email: " + email);
+
+        BottomNavigationView bottomvan = findViewById(R.id.Bottom_Navigation);
+        bottomvan.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+                if (menuItem.getItemId() == R.id.Bottom_Home){
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if (menuItem.getItemId() == R.id.Bottom_Traps){
+                    Intent intent = new Intent(getBaseContext(), TrapList.class);
+                    intent.putExtra("trapListPassedToIntent", trapList);
+                    //Log.d("SERVIISI3","Intenttii menevä listan size: " + trapList.size());
+                    startActivity(intent);
+                    return true;
+                }
+                else if (menuItem.getItemId() == R.id.Bottom_Post){
+                    Intent intent = new Intent(getBaseContext(), NewPostActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
